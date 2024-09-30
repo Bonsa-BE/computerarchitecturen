8d0d4f3b377dc518d743d4a867f4270570a21636
Toon Van Havermaet <toon.vanhavermaet@student.odisee.be>
2024-09-30 14:53:25 +0200

    excercise lab2

--- lab2/src/main.cpp
+++ lab2/src/main.cpp
@@ -1,27 +1,15 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <myArduino.h>
 
 int main(void)
 {
-  DDRB = (1 >> DDB5) | DDRB;
-  while (1)
+ pinMode(13, OUTPUT);
+  while (1):
   {
-    /*toggle led on*/
-    PORTB = (1 << PB5) | PORTB;
-    /*sleep*/
+    digitalWrite(13, HIGH);
     _delay_ms(1000);
-    /*toggle led off*/
-    PORTB = PORTB & ~(1 << PB5);
-    /*foute lijn, verandert waarden aan register: PORTB = (0 << PB5) | PORTB;*/
-    /*sleep*/
+    digitalWrite(13, LOW);
     _delay_ms(1000);
   }
 }
-
-/*The DDxn bit in the DDRx register selects the direction of this pin. If DDxn is written logic one, Pxn is configured as an output
-pin. If DDxn is written logic zero, Pxn is configured as an input pin.
-If PORTxn is written logic one when the pin is configured as an input pin, the pull-up resistor is activated. To switch the
-pull-up resistor off, PORTxn has to be written logic zero or the pin has to be configured as an output pin. The port pins are
-tri-stated when reset condition becomes active, even if no clocks are running.
-If PORTxn is written logic one when the pin is configured as an output pin, the port pin is driven high (one). If PORTxn is
-written logic zero when the pin is configured as an output pin, the port pin is driven low (zero).*/
