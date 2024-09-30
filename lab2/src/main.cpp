cd751bab7e7a1cb4c877c61dda06f06444f3c73c
Toon Van Havermaet <toon.vanhavermaet@student.odisee.be>
2024-09-30 15:01:17 +0200

    trying out stuff

--- lab2/src/main.cpp
+++ lab2/src/main.cpp
@@ -1,11 +1,11 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include <myArduino.h>
+#include "../include/myArduino.h"
 
 int main(void)
 {
  pinMode(13, OUTPUT);
-  while (1):
+  while (1)
   {
     digitalWrite(13, HIGH);
     _delay_ms(1000);
