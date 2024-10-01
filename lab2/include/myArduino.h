69a339838a5c4581cb18ae7298e117cd82497b82
Toon Van Havermaet <toon.vanhavermaet@student.odisee.be>
2024-10-01 16:08:20 +0200

    finished first part of lab2

--- lab2/include/myArduino.h
+++ lab2/include/myArduino.h
@@ -1,21 +1,45 @@
-#ifndef Arduino_h
-#define Arduino_h
+#ifndef myArduino_h
+#define myArduino_h
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 #include <math.h>
+#include <stdio.h>
 
 #include <avr/pgmspace.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
 #define HIGH 0x1
-#define LOW  0x0
+#define LOW 0x0
 
 #define INPUT 0x0
 #define OUTPUT 0x1
 #define INPUT_PULLUP 0x2
 
-void pinMode(uint8_t pin, uint8_t mode);
-void digitalWrite(uint8_t pin, uint8_t val);
+void pinMode(volatile uint8_t *pin, uint8_t mode)
+{
+    if (mode == OUTPUT)
+    {
+        *pin = *pin | (1 << 5);
+    }
+
+    else if (mode == INPUT)
+    {
+        *pin = *pin & ~(1 << 5);
+    }
+}
+
+void digitalWrite(volatile uint8_t *pin, uint8_t val)
+{
+    if (val == HIGH)
+    {
+        *pin = *pin | (1 << 5);
+    }
+
+    else if (val == LOW)
+    {
+        *pin = *pin & ~(1 << 5);
+    }
+}
 #endif
--- lab2/src/main.cpp
+++ lab2/src/main.cpp
@@ -1,6 +1,6 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include <myArduino.h>
+#include "myArduino.h"
 /*
 portB data register: 0x24 (hoog/laag)
 portB DDR : 0x25 (input/output)
@@ -8,16 +8,18 @@ portB input register : 0x23 (uitlezen waarde)
 */
 int main(void)
 {
-  pinMode(0x25, (1 << DDB5) | DDRB);
+  volatile uint8_t *ptr = (volatile uint8_t *)0x24;
+  volatile uint8_t *ptr1 = (volatile uint8_t *)0x25;
+  pinMode(ptr, OUTPUT);
   while (1)
   {
     /*toggle led on*/
     /* PORTB = (1 << PB5) | PORTB;*/
-    digitalWrite(0x24, (1 << PB5) | PORTB);
+    digitalWrite(ptr1, HIGH);
     /*sleep*/
     _delay_ms(1000);
     /*toggle led off*/
-    digitalWrite(0x24, PORTB & ~(1 << PB5));
+    digitalWrite(ptr1, LOW);
     /*PORTB = PORTB & ~(1 << PB5);*/
     /*foute lijn, verandert waarden aan register: PORTB = (0 << PB5) | PORTB;*/
     /*sleep*/
