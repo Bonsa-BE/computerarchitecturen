0da3249ce34090916b332705d61c490d9c812758
Bonsa-BE <toon.vh12@gmail.com>
2024-10-08 16:27:21 +0200

    improve comments

--- lab3/src/main.cpp
+++ lab3/src/main.cpp
@@ -9,7 +9,7 @@ ISR(INT1_vect) //create ISR for INT1 (pin 3)
   ledstate = HIGH;
   digitalWrite(LED_BUILTIN, ledstate);
 }
-ISR(INT0_vect){ //creaet ISR for PCINT0 (pin 2)
+ISR(INT0_vect){ //create ISR for INT0 (pin 2)
   ledstate = LOW;
   digitalWrite(LED_BUILTIN, ledstate);
 }
@@ -21,12 +21,13 @@ pinMode(2, INPUT_PULLUP);
 pinMode(3, INPUT_PULLUP);
 
   // Enable Pin Change Mask for arduino pin 2 and 3
-  EICRA |= (1 << ISC11);
-  EIMSK |= (1<< INT0);
+  EICRA |= (1 << ISC11); //falling edge detection
+  EIMSK |= (1<< INT0); //enable INT0 (pin 2)
 
-  EICRA |= (1<< ISC01);
-  EIMSK |= (1<< INT1);
-  sei();
+  EICRA |= (1<< ISC01); //falling edge detection
+  EIMSK |= (1<< INT1); //enable INT1 (pin 3)
+
+  sei(); //enable global interrupts
   while (1)
   {
     // do nothing
