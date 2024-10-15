0ac58c45099052603ff93441b754b377ceb4e047
Bonsa-BE <toon.vh12@gmail.com>
2024-10-15 14:13:28 +0200

    update bug in code

--- lab3/src/main.cpp
+++ lab3/src/main.cpp
@@ -21,11 +21,11 @@ pinMode(2, INPUT_PULLUP);
 pinMode(3, INPUT_PULLUP);
 
   // Enable Pin Change Mask for arduino pin 2 and 3
-  EICRA |= (1 << ISC11); //falling edge detection
-  EIMSK |= (1<< INT0); //enable INT0 (pin 2)
+  EICRA |= (1<< ISC11); //falling edge detection (alternatief: EICRA |= B00000100)
+  EIMSK |= (1<< INT1); //enable INT0 (pin 2)
 
   EICRA |= (1<< ISC01); //falling edge detection
-  EIMSK |= (1<< INT1); //enable INT1 (pin 3)
+  EIMSK |= (1<< INT0); //enable INT1 (pin 3)
 
   sei(); //enable global interrupts
   while (1)
