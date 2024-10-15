41c845f5fe9719d03c2f4c52c20a09f7b641a512
Bonsa-BE <toon.vh12@gmail.com>
2024-10-15 16:12:23 +0200

    lab4 finished

--- lab4/src/main.cpp
+++ lab4/src/main.cpp
@@ -1,18 +1,35 @@
-#include <Arduino.h>
+#include <arduino.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ISR(PCINT2_vect) // create ISR for PCINT1 (PCINT8-PCINT14)
+{
+  if (digitalRead(PD5) == LOW)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else if (digitalRead(PD7) == LOW)
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+int main()
+{
+  pinMode(LED_BUILTIN, OUTPUT); // pin gpio13
+  pinMode(PD7, INPUT_PULLUP);   // pin gpio7
+  pinMode(PD5, INPUT_PULLUP);   // pin gpio5
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  PCICR |= (1 << PCIE2);    // enable logic change detection for INT2 vector
+  PCMSK2 |= (1 << PCINT21) | (1 << PCINT23); // enable interrupt on pcint21 (pin 5) and pcint23 (pin 7)
+
+  sei(); // enable global interrupts
+
+  while (1)
+  {
+    // do nothing
+  }
+  return 0;
 }
