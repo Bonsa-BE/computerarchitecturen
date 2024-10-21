8f507ea1dfd0e270adc618c199ddbc79a598a9a4
Bonsa-BE <toon.vh12@gmail.com>
2024-10-21 09:03:42 +0200

    update lab4 to internal led of dramco uno

--- lab4/src/main.cpp
+++ lab4/src/main.cpp
@@ -6,21 +6,21 @@ ISR(PCINT2_vect) // create ISR for PCINT1 (PCINT8-PCINT14)
 {
   if (digitalRead(PD5) == LOW)
   {
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(PB2, HIGH);
   }
   else if (digitalRead(PD7) == LOW)
   {
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(PB2, LOW);
   }
 }
 
 int main()
 {
-  pinMode(LED_BUILTIN, OUTPUT); // pin gpio13
+  pinMode(PB2, OUTPUT); // pin gpio10
   pinMode(PD7, INPUT_PULLUP);   // pin gpio7
   pinMode(PD5, INPUT_PULLUP);   // pin gpio5
 
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(PB2, HIGH);
 
   PCICR |= (1 << PCIE2);  // enable logic change detection for INT2 vector
   PCMSK2 |= (1 << PCINT21) | (1 << PCINT23);  // enable interrupt on pcint21 (pin 5) and pcint23 (pin 7)
